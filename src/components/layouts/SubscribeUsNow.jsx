9bbd3e4e7e472e9b2eaaa0b9e60c15bce4356d2d
thuong2106 <phamhoaithuong2106@gmail.com>
2024-10-22 02:19:55 +0700

    Update SubscribeUsNow

--- src/components/layouts/SubscribeUsNow.jsx
+++ src/components/layouts/SubscribeUsNow.jsx
@@ -12,29 +12,29 @@ const SubscribeUsNow = () => {
     };
 
     return (
-        <div className="flex items-center justify-center p-10 pt-[100px] rounded-lg shadow-md relative">
+        <div className="flex items-center justify-center p-10 pt-[100px] rounded-lg relative bg-white">
             <img
                 src="imgs/bg5.png"
                 alt="Background"
-                className="absolute w-[280px] ml-[500px] object-cover"
+                className="absolute hidden lg:block w-[280px] ml-[500px] object-cover" 
             />
-            <div className="flex flex-col items-center justify-center z-10">
+            <div className="flex flex-col items-center justify-center z-10 max-w-full sm:max-w-md">
                 <h2 className="text-2xl font-bold mb-2 text-center">Đăng Ký Với Chúng Tôi Ngay</h2>
-                <span className="mb-4 text-gray-700">Nhận thêm tin tức và món ăn ngon mỗi ngày từ chúng tôi</span>
-                <div className="flex items-center border rounded-md overflow-hidden">
+                <span className="mb-4 text-gray-700 text-center">Nhận thêm tin tức và món ăn ngon mỗi ngày từ chúng tôi</span>
+                <div className="flex items-center border rounded-md overflow-hidden w-full max-w-[400px]">
                     <input
                         type="email"
                         placeholder="Email"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                        className="p-2 flex-1 border-none focus:outline-none"
+                        className="p-2 flex-1 border-none focus:outline-none text-sm"
                         required
-                    /> |
+                    />
                     <button
                         onClick={handleSubscribe}
                         className="flex items-center justify-center text-white p-2 transition-colors duration-300"
                     >
-                        <FaPaperPlane style={{color: colorCode}}/>
+                        <FaPaperPlane style={{color: colorCode}} />
                     </button>
                 </div>
             </div>
