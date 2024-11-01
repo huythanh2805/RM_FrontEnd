a5e994d971718334a15a8af64fb19ae83425be2e
caosontr <caosontr@gmail.com>
2024-11-01 18:29:30 +0700

    create employee

---
+++ src/pages/dashboard/employee/EmployeeAdd.jsx
@@ -0,0 +1,176 @@
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "@/hooks/use-toast";
+
+const EmployeeAdd = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  const onSubmit = (data) => {
+    axios
+      .post("http://localhost:1111/employees", data)
+      .then(() => {
+        navigate("/dashboard/employees");
+        toast({ variant: "success", title: "Thêm nhân viên thành công!" });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="w-full min-h-screen bg-[#f5f6fa]">
+      <div className="px-5 py-2">
+        <h2 className="text-[32px] font-semibold mb-4">Thêm nhân viên</h2>
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor="name" className="text-sm font-medium text-gray-700">
+              Tên nhân viên:
+            </label>
+            <input
+              type="text"
+              className={`mt-1 block w-full px-4 py-2 border ${
+                errors.name
+                  ? "border-red-500 focus:border-red-500"
+                  : "border-gray-300 focus:border-gray-500"
+              } rounded-md shadow-sm focus:outline-none`}
+              {...register("name", {
+                required: "Tên là bắt buộc",
+                minLength: {
+                  value: 3,
+                  message: "Tên phải có ít nhất 3 ký tự",
+                },
+              })}
+            />
+            {errors.name && (
+              <p className="mt-2 text-sm text-red-600">{errors.name.message}</p>
+            )}
+          </div>
+
+          <div>
+            <label
+              htmlFor="gender"
+              className="text-sm font-medium text-gray-700"
+            >
+              Giới tính:
+            </label>
+            <select
+              className="mt-1 block w-full px-4 py-2 border rounded-md shadow-sm focus:outline-none"
+              {...register("gender", { required: "Giới tính là bắt buộc" })}
+            >
+              <option value="">Chọn giới tính</option>
+              <option value="MALE">MALE</option>
+              <option value="FEMALE">FEMALE</option>
+            </select>
+            {errors.gender && (
+              <p className="mt-2 text-sm text-red-600">
+                {errors.gender.message}
+              </p>
+            )}
+          </div>
+
+          <div>
+            <label
+              htmlFor="phoneNumber"
+              className="text-sm font-medium text-gray-700"
+            >
+              Psssword:
+            </label>
+            <input
+              type="text"
+              className={`mt-1 block w-full px-4 py-2 border ${
+                errors.password
+                  ? "border-red-500 focus:border-red-500"
+                  : "border-gray-300 focus:border-gray-500"
+              } rounded-md shadow-sm focus:outline-none`}
+              {...register("password", {
+                required: "Mật khẩu là bắt buộc",
+                pattern: {
+                  message: "Mật khẩu",
+                },
+              })}
+            />
+            {errors.password && (
+              <p className="mt-2 text-sm text-red-600">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="phoneNumber"
+              className="text-sm font-medium text-gray-700"
+            >
+              Số điện thoại:
+            </label>
+            <input
+              type="text"
+              className={`mt-1 block w-full px-4 py-2 border ${
+                errors.phoneNumber
+                  ? "border-red-500 focus:border-red-500"
+                  : "border-gray-300 focus:border-gray-500"
+              } rounded-md shadow-sm focus:outline-none`}
+              {...register("phoneNumber", {
+                required: "Số điện thoại là bắt buộc",
+                pattern: {
+                  value: /^[0-9]{10}$/,
+                  message: "Số điện thoại không hợp lệ",
+                },
+              })}
+            />
+            {errors.phoneNumber && (
+              <p className="mt-2 text-sm text-red-600">
+                {errors.phoneNumber.message}
+              </p>
+            )}
+          </div>
+
+          <div>
+            <label
+              htmlFor="workPosition"
+              className="text-sm font-medium text-gray-700"
+            >
+              Vị trí công việc:
+            </label>
+            <input
+              type="text"
+              className="mt-1 block w-full px-4 py-2 border rounded-md shadow-sm focus:outline-none"
+              {...register("workPosition", {
+                required: "Vị trí công việc là bắt buộc",
+              })}
+            />
+            {errors.workPosition && (
+              <p className="mt-2 text-sm text-red-600">
+                {errors.workPosition.message}
+              </p>
+            )}
+          </div>
+
+          <div className="flex justify-end space-x-2">
+            <Link
+              to="/dashboard/employees"
+              className="bg-gray-200 text-gray-800 px-6 py-2 rounded-md text-sm font-semibold hover:bg-gray-300"
+            >
+              Quay lại
+            </Link>
+
+            <button
+              type="submit"
+              className="bg-green-200 text-green-800 px-6 py-2 rounded-md text-sm font-semibold hover:bg-green-300 transition"
+            >
+              Thêm +
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeAdd;
---
+++ src/pages/dashboard/employee/EmployeeList.jsx
@@ -0,0 +1,152 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { FaPenToSquare, FaRegTrashCan } from "react-icons/fa6";
+import Swal from "sweetalert2";
+import Pagination from "@/components/Pagination";
+
+const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemPerPage = 5;
+
+  const fetchData = () => {
+    axios
+      .get("http://localhost:1111/employees")
+      .then((res) => {
+        setEmployees(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const pageCount = Math.ceil(employees.length / itemPerPage);
+
+  const currentItems = employees.slice(
+    currentPage * itemPerPage,
+    (currentPage + 1) * itemPerPage
+  );
+
+  const handlePageChange = (e) => {
+    setCurrentPage(e.selected);
+  };
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Xác nhận xóa ?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Xác nhận",
+      cancelButtonText: "Hủy",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios
+          .delete(`http://localhost:1111/employees/${id}`)
+          .then(() => {
+            Swal.fire(
+              "Đã xóa!",
+              "Nhân viên đã được xóa thành công.",
+              "success"
+            );
+            fetchData();
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    });
+  };
+
+  return (
+    <div className="w-full min-h-screen bg-[#f5f6fa]">
+      <div className="px-5 py-2">
+        <div className="flex items-center justify-between">
+          <p className="text-[32px] font-semibold mb-4">Nhân Viên</p>
+          <Link to={"/dashboard/employees/add"}>
+            <div className="bg-green-200 text-green-800 px-6 py-2 rounded-md text-xs font-semibold hover:bg-green-300 transition">
+              Thêm +
+            </div>
+          </Link>
+        </div>
+        <div className="overflow-x-auto rounded-xl border border-[#d5d5d5]">
+          <table className="min-w-full bg-white">
+            <thead className="border-b border-[#d5d5d5] text-left text-xs font-semibold text-[#202224] uppercase tracking-wider">
+              <tr>
+                <th className="hidden lg:block py-3 px-6">STT</th>
+                <th className="py-3 px-6">Tên</th>
+                <th className="py-3 px-6">Giới tính</th>
+                <th className="py-3 px-6">Số điện thoại</th>
+                <th className="py-3 px-6">Vị trí công việc</th>
+                <th className="py-3 px-6">Trạng thái</th>
+                <th className="py-3 px-6">Lịch làm việc</th>
+                <th className="py-3 px-6"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentItems.map((d, index) => (
+                <tr
+                  className="bg-white border-b border-[#d5d5d5] hover:bg-gray-50 transition"
+                  key={d._id}
+                >
+                  <td className="hidden lg:block py-4 px-6 text-sm font-medium text-[#202224]">
+                    {index + 1}
+                  </td>
+                  <td className="py-4 px-6 text-sm font-medium text-[#202224]">
+                    {d.name}
+                  </td>
+                  <td className="py-4 px-6 text-sm font-medium text-[#202224]">
+                    {d.gender}
+                  </td>
+                  <td className="py-4 px-6 text-sm font-medium text-[#202224]">
+                    {d.phoneNumber}
+                  </td>
+                  <td className="py-4 px-6 text-sm font-medium text-[#202224]">
+                    {d.workPosition}
+                  </td>
+
+                  <td className="py-4 px-6 text-sm">
+                    {d.employStatus === "ACTIVE" ? (
+                      <span className="px-2 py-1 text-xs font-semibold rounded-lg bg-green-100 text-green-800">
+                        ACTIVE
+                      </span>
+                    ) : (
+                      <span className="px-2 py-1 text-xs font-semibold rounded-lg bg-gray-100 text-gray-700">
+                        LEAVED
+                      </span>
+                    )}
+                  </td>
+                  <td className="py-4 px-6 text-sm font-medium text-[#202224]">
+                    Trống
+                  </td>
+                  <td className="py-4 px-6 text-sm flex items-center gap-1.5 lg:gap-3">
+                    <Link to={`/dashboard/employees/${d._id}/update`}>
+                      <div className="bg-blue-200 text-blue-800 px-3 py-1 rounded-lg text-xs lg:text-base font-semibold hover:bg-blue-300 transition">
+                        <FaPenToSquare />
+                      </div>
+                    </Link>
+                    <div
+                      className="bg-red-200 text-red-800 px-3 py-1 rounded-lg cursor-pointer text-xs lg:text-base font-semibold hover:bg-red-300 transition"
+                      onClick={() => handleDelete(d._id)}
+                    >
+                      <FaRegTrashCan />
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Pagination pageCount={pageCount} onPageChange={handlePageChange} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeList;
