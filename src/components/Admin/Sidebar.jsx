72ca71089378fe1a6df2a1269ffd1f237155625e
caosontr <caosontr@gmail.com>
2024-11-18 18:19:46 +0700

    update sidebar shadcn-ui

--- src/components/Admin/Sidebar.jsx
+++ src/components/Admin/Sidebar.jsx
@@ -1,231 +1,113 @@
-import React, { useState } from "react";
-import { AiFillGolden, AiFillProduct } from "react-icons/ai";
-import { FaHome, FaRegMoneyBillAlt } from "react-icons/fa";
-import { FaUserGroup } from "react-icons/fa6";
-import { MdCategory, MdRememberMe } from "react-icons/md";
-import { SiAirtable } from "react-icons/si";
-import { Link } from "react-router-dom";
+import {
+  Home,
+  User,
+  Grid,
+  Soup,
+  Layers,
+  Table,
+  Contact,
+  DollarSign,
+  ChevronDown,
+} from "lucide-react";
 
-const Sidebar = () => {
-  const [isCollapsed, setIsCollapsed] = useState(false);
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSubItem,
+  SidebarMenuSub,
+  SidebarGroupLabel,
+} from "@/components/ui/sidebar";
 
-  const toggleSidebar = () => {
-    setIsCollapsed((prev) => !prev);
-  };
+import {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from "@radix-ui/react-collapsible";
+
+export function AppSidebar() {
+  const menuItems = [
+    { title: "Home", url: "/admin", icon: Home },
+    { title: "Account", url: "/admin/users", icon: User },
+    { title: "Categories", url: "/admin/categories", icon: Grid },
+    { title: "Dishes", url: "/admin/dishes", icon: Soup },
+    { title: "Combo", url: "/admin/setCombos", icon: Layers },
+    { title: "Table", url: "/admin/tables", icon: Table },
+    { title: "Employee", url: "/admin/employees", icon: Contact },
+    { title: "Bills", url: "/admin/bills", icon: DollarSign },
+  ];
+
+  const SubItems = [
+    { title: "Item 1", url: "/" },
+    { title: "Item 2", url: "/" },
+  ];
 
   return (
-    <div
-      className={`transition-all duration-300 ${
-        isCollapsed ? "w-28" : "xl:w-80 w-80"
-      } xl:p-4 p-2 bg-white flex-col justify-start items-start gap-5 inline-flex border-r`}
-    >
-      <div className="w-full pt-4 justify-between items-center gap-2.5 inline-flex">
-        <Link to="/" className="flex flex-row items-center cursor-pointer">
+    <Sidebar>
+      <SidebarHeader>
+        <div className="flex items-center gap-2 p-4">
           <img
             src="/imgs/logoGolden.webp"
             alt="Golden Fork Logo"
             className="h-16 w-16 object-cover rounded-full"
           />
-          {!isCollapsed && (
-            <h1 className="text-xl font-semibold font-serif">Golden Fork</h1>
-          )}
-        </Link>
-        <a
-          href="javascript:;"
-          className="w-6 h-6 relative bg-white"
-          onClick={toggleSidebar}
-        >
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g id="Menu">
-              <rect width="24" height="24" fill="white" />
-              <path
-                id="icon"
-                d="M13 6H21M3 12H21M7 18H21"
-                stroke="#1F2937"
-                strokeWidth="1.6"
-                strokeLinecap="round"
-              />
-            </g>
-          </svg>
-        </a>
-      </div>
+          <span className="font-bold text-lg">Golden Fork</span>
+        </div>
+      </SidebarHeader>
 
-      {/* Menu */}
-      <div className="w-full">
-        <ul
-          className={`flex-col gap-1 flex ${isCollapsed ? "items-center" : ""}`}
-        >
-          <li>
-            <Link to="/admin">
-              <div className="flex-col flex p-3 bg-white rounded-lg">
-                <div className="h-5 gap-3 flex items-center">
-                  <FaHome />
-                  {!isCollapsed && (
-                    <h2 className="text-gray-500 text-sm font-medium leading-snug">
-                      Home
-                    </h2>
-                  )}
-                </div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/users">
-              <div className="flex-col gap-1 flex">
-                <div className="flex-col flex p-3 bg-white rounded-lg">
-                  <div className="h-5 gap-3 flex">
-                    <div className="relative">
-                      <FaUserGroup />
-                    </div>
-                    {!isCollapsed && (
-                      <h2 className="text-gray-500 text-sm font-medium leading-snug">
-                        Account
-                      </h2>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/categories">
-              <div className="flex-col gap-1 flex">
-                <div className="flex-col flex p-3 bg-white rounded-lg">
-                  <div className="h-5 gap-3 flex">
-                    <div className="relative">
-                      <MdCategory />
-                    </div>
-                    {!isCollapsed && (
-                      <h2 className="text-gray-500 text-sm font-medium leading-snug">
-                        Categories
-                      </h2>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </li>
-
-          <li>
-            <Link to={"/admin/dishes"}>
-              <div className="flex-col flex">
-                <div className="flex-col flex p-3 bg-white rounded-lg">
-                  <div className="justify-between inline-flex">
-                    <a href="javascript:;" className="h-5 gap-3 flex">
-                      <div className="relative">
-                        <AiFillProduct />
-                      </div>
-                      {!isCollapsed && (
-                        <h2 className="text-gray-500 text-sm font-medium leading-snug">
-                          Dishes
-                        </h2>
-                      )}
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {menuItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton className="my-2">
+                    <a href={item.url} className="flex items-center gap-2">
+                      <item.icon className="mr-2" />
+                      <span>{item.title}</span>
                     </a>
-                  </div>
-                </div>
-              </div>
-            </Link>
-            {/* <div className="flex-col flex">
-              <div className="flex-col flex p-3 bg-white rounded-lg">
-                <div className="justify-between inline-flex">
-                  <a href="javascript:;" className="h-5 gap-3 flex">
-                    <div className="relative">
-                      <AiFillProduct />
-                    </div>
-                    <h2 className="text-gray-500 text-sm font-medium leading-snug">
-                      Dishes
-                    </h2>
-                  </a>
-                </div>
-              </div>
-            </div> */}
-          </li>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
 
-          <li>
-            <Link to={"/admin/setCombos"}>
-              <div className="flex-col flex">
-                <div className="flex-col flex p-3 bg-white rounded-lg">
-                  <div className="justify-between inline-flex">
-                    <a href="javascript:;" className="h-5 gap-3 flex">
-                      <div className="relative">
-                        <AiFillGolden />
+              <Collapsible defaultOpen className="group/collapsible">
+                <SidebarMenuItem>
+                  <SidebarMenuButton className="my-2">
+                    <CollapsibleTrigger className="flex items-center justify-between w-full">
+                      <div className="flex items-center gap-2">
+                        <Contact className="mr-2" />
+                        <span>Item</span>
                       </div>
-                      {!isCollapsed && (
-                        <h2 className="text-gray-500 text-sm font-medium leading-snug">
-                          Combo
-                        </h2>
-                      )}
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </li>
+                      <ChevronDown className="transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                    </CollapsibleTrigger>
+                  </SidebarMenuButton>
 
-          <li>
-            <Link to="/admin/tables">
-              <div className="flex-col gap-1 flex">
-                <div className="flex-col flex bg-white rounded-lg p-3">
-                  <div className="h-5 gap-3 flex">
-                    <div className="relative">
-                      <SiAirtable />
-                    </div>
-                    {!isCollapsed && (
-                      <h2 className="text-gray-500 text-sm font-medium leading-snug">
-                        Table
-                      </h2>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/employees">
-              <div className="flex-col gap-1 flex">
-                <div className="flex-col flex bg-white rounded-lg p-3">
-                  <div className="h-5 gap-3 flex">
-                    <div className="relative">
-                      <MdRememberMe />
-                    </div>
-                    {!isCollapsed && (
-                      <h2 className="text-gray-500 text-sm font-medium leading-snug">
-                        Employee
-                      </h2>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/bills">
-              <div className="flex-col gap-1 flex">
-                <div className="flex-col flex bg-white rounded-lg p-3">
-                  <div className="h-5 gap-3 flex">
-                    <div className="relative">
-                      <FaRegMoneyBillAlt />
-                    </div>
-                    {!isCollapsed && (
-                      <h2 className="text-gray-500 text-sm font-medium leading-snug">
-                        Bills
-                      </h2>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {SubItems.map((subItem) => (
+                        <SidebarMenuSubItem key={subItem.title}>
+                          <a
+                            href={subItem.url}
+                            className="ml-6 flex items-center gap-2"
+                          >
+                            <span>{subItem.title}</span>
+                          </a>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                    <SidebarGroupContent />
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
   );
-};
-
-export default Sidebar;
+}
