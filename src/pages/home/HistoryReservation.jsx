e069759ecfdacc9fc7dc14308a9b91471dfa48d9
caosontr <caosontr@gmail.com>
2024-12-15 14:10:32 +0700

    Fix Mobile UI HistoryReservation

--- src/pages/home/HistoryReservation.jsx
+++ src/pages/home/HistoryReservation.jsx
@@ -2,40 +2,40 @@ import ButtonCustome from "@/components/ButtonCustome";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import jwtDecode from "jwt-decode";
-import { useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { io } from "socket.io-client";
 
 export const HistoryReservation = () => {
-  // const { userId } = useParams();
-  const [userId, setUserId] = useState(()=>{
-      const token = localStorage.getItem('token')
-      return jwtDecode(token).id
-    })
-  const queryClient = useQueryClient();
-  const [opacity] = useState(1);
-  const [translateY] = useState(0);
-  const navigate = useNavigate();
-  // Fetch danh sách đặt bàn
-  const { data, error, isLoading } = useQuery(["reservations", userId], async () => {
-    const response = await axios.get(`http://localhost:1111/api/reservations/user/${userId}`);
-
-    return response.data;
+  const [userId, setUserId] = useState(() => {
+    const token = localStorage.getItem("token");
+    return jwtDecode(token).id;
   });
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  // Fetch danh sách đặt bàn
+  const { data, error, isLoading } = useQuery(
+    ["reservations", userId],
+    async () => {
+      const response = await axios.get(
+        `http://localhost:1111/api/reservations/user/${userId}`
+      );
+      return response.data;
+    }
+  );
+
   useEffect(() => {
     const socket = io("http://localhost:1111"); // URL của server WebSocket
 
-    // Lắng nghe sự kiện "reservation-canceled"
     socket.on("new-notification", (data) => {
-      setReservations((prev) =>
-        prev.map((res) => (res._id === data.reservationId ? { ...res, status: data.status } : res))
-      );
+      queryClient.invalidateQueries(["reservations", userId]);
     });
 
     return () => {
       socket.disconnect();
     };
-  }, []);
+  }, [queryClient, userId]);
 
   // Mutation để hủy đặt bàn
   const [isCanceling, setIsCanceling] = useState(false);
@@ -62,10 +62,31 @@ export const HistoryReservation = () => {
       },
       onError: (error) => {
         setIsCanceling(false);
-        alert(error.response?.data?.message || "Đã xảy ra lỗi khi hủy đơn hàng.");
+        alert(
+          error.response?.data?.message || "Đã xảy ra lỗi khi hủy đơn hàng."
+        );
       },
     }
   );
+
+  const getStatusStyle = (status) => {
+    switch (status) {
+      case "ISWAITING":
+        return "bg-yellow-100 text-yellow-800"; // Đang chờ
+      case "ISCOMFIRMED":
+        return "bg-blue-100 text-blue-800"; // Đã xác nhận
+      case "SEATED":
+        return "bg-purple-100 text-purple-800"; // Đã ngồi
+      case "COMPLETED":
+        return "bg-green-100 text-green-800"; // Hoàn thành
+      case "CANCELED":
+        return "bg-red-100 text-red-800"; // Đã hủy
+      case "ISPAYMENT":
+        return "bg-purple-200 text-purple-800"; // Chờ thanh toán
+      default:
+        return "bg-gray-100 text-gray-800"; // Không xác định
+    }
+  };
 
   const handleCancelReservation = (reservationId) => {
     if (isCanceling) {
@@ -77,9 +98,11 @@ export const HistoryReservation = () => {
       mutation.mutate(reservationId);
     }
   };
+
   const handleBack = (code) => {
     navigate(`/payment?code=${code}`);
   };
+
   // Chuyển đổi trạng thái sang tiếng Việt
   const getStatusInVietnamese = (status) => {
     switch (status) {
@@ -103,7 +126,7 @@ export const HistoryReservation = () => {
   return (
     <div>
       {/* Banner */}
-      <div className="relative w-full h-[200px] overflow-hidden">
+      <div className="relative w-full h-[150px] sm:h-[100px] overflow-hidden">
         <div
           className="absolute inset-0 bg-cover bg-center"
           style={{
@@ -113,27 +136,13 @@ export const HistoryReservation = () => {
           }}
         ></div>
         <div className="absolute inset-0 bg-black opacity-30"></div>
-        <div
-          className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white"
-          style={{
-            opacity: opacity,
-            transform: `translateY(-${translateY}px)`,
-            transition: "opacity 0.3s, transform 0.3s",
-          }}
-        >
-          <h1 className="text-4xl md:text-5xl sm:text-3xl dancing">Lịch sử đặt bàn</h1>
-          <p className="text-3xl md:text-[20px] sm:text-[15px] mt-4 flex items-center justify-center">
-            <span className="bg-white p-1 rounded-full ml-0 mr-0 hidden lg:block"></span>
-            <span className="bg-white h-[2px] w-[100px] hidden lg:block"></span>
-            <span className="ml-4">Khám phá tất cả các lần đặt bàn trước đây của bạn</span>
-            <span className="bg-white h-[2px] w-[100px] ml-4 hidden lg:block"></span>
-            <span className="bg-white p-1 rounded-full ml-0 mr-0 hidden lg:block"></span>
-          </p>
+        <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
+          <h1 className="text-3xl sm:text-2xl">Lịch sử đặt bàn</h1>
         </div>
       </div>
 
       {/* Lịch sử đặt bàn */}
-      <section className="py-24 relative">
+      <section className="py-8 relative">
         <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
           {!data?.reservations ? (
             <div className="text-center text-2xl text-gray-500 mt-4">
@@ -145,111 +154,66 @@ export const HistoryReservation = () => {
               </div>
             </div>
           ) : (
-            <table className="min-w-full mt-6 bg-gray-50 border border-gray-200">
-              <thead>
-                <tr className="bg-gray-200 text-gray-500 text-base font-normal leading-relaxed">
-                  <th className="py-4 px-6 text-left">Thông tin</th>
-                  <th className="py-4 px-6 text-center">Ngày đặt</th>
-                  <th className="py-4 px-6 text-center">Giờ đặt</th>
-                  <th className="py-4 px-6 text-center">Số người</th>
-                  <th className="py-4 px-6 text-center">Trạng thái</th>
-                  <th className="py-4 px-6 text-center"></th>
-                </tr>
-              </thead>
-              <tbody>
-                {data?.reservations?.map((reservation) => {
-                  const date = new Date(reservation.startTime);
-                  const formattedDate = date.toLocaleDateString("vi-VN");
-                  const formattedTime = date.toLocaleTimeString("vi-VN", { hour: "2-digit", minute: "2-digit" });
+            <div className="space-y-4">
+              {data?.reservations?.map((reservation) => {
+                const date = new Date(reservation.startTime);
+                const formattedDate = date.toLocaleDateString("vi-VN");
+                const formattedTime = date.toLocaleTimeString("vi-VN", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                });
 
-                  let statusClass = "";
-                  switch (reservation.status) {
-                    case "ISWAITING":
-                      statusClass = "bg-amber-50 text-amber-600 border border-amber-400";
-                      break;
-                    case "ISCOMFIRMED":
-                      statusClass = "bg-green-200 text-green-600 border border-green-400";
-                      break;
-                    case "SEATED":
-                      statusClass = "bg-green-200 text-green-600 border border-green-400";
-                      break;
-                    case "ISPAYMENT":
-                      statusClass = "bg-amber-50 text-amber-600 border border-amber-400";
-                      break;
-                    case "COMPLETED":
-                      statusClass = "bg-green-200 text-green-600 border border-green-400";
-                      break;
-                    case "CANCELED":
-                      statusClass = "bg-red-50 text-red-600 border border-red-400";
-                      break;
-                    default:
-                      statusClass = "bg-gray-50 text-gray-600 border border-gray-400";
-                  }
+                return (
+                  <div
+                    key={reservation._id}
+                    className="bg-white shadow-lg rounded-lg p-6 border border-gray-200 hover:shadow-xl transition-all"
+                  >
+                    <div className="flex justify-between items-center">
+                      <h4 className="text-xl font-semibold text-gray-800">
+                        <strong>Người đặt:</strong> {reservation.userName}
+                      </h4>
+                      <div
+                        className={`p-3 text-sm font-medium rounded-lg inline-flex items-center ${getStatusStyle(
+                          reservation.status
+                        )} shadow-md transition duration-300 ease-in-out hover:shadow-xl`}
+                      >
+                        {getStatusInVietnamese(reservation.status)}
+                      </div>
+                    </div>
+                    <p className="mt-2 text-gray-600">
+                      <strong>Số điện thoại:</strong> {reservation.phoneNumber}
+                    </p>
+                    <p className="mt-1 text-gray-600">
+                      <strong>Ngày đặt:</strong> {formattedDate}
+                    </p>
+                    <p className="mt-1 text-gray-600">
+                      <strong>Giờ đặt:</strong> {formattedTime}
+                    </p>
+                    <p className="mt-1 text-gray-600">
+                      <strong>Số người:</strong> {reservation.guests_count}
+                    </p>
 
-                  return (
-                    <tr key={reservation._id} className="border-b border-gray-200">
-                      <td className="py-4 px-6 flex flex-col items-start">
-                        <h4 className="text-black text-lg font-medium leading-8">{reservation.userName}</h4>
-                        <h4 className="text-black text-lg font-medium leading-8">{reservation.phoneNumber}</h4>
-                      </td>
-                      <td className="py-4 px-6 text-center text-black text-lg font-medium leading-relaxed">
-                        {formattedDate}
-                      </td>
-                      <td className="py-4 px-6 text-center text-black text-lg font-medium leading-relaxed">
-                        {formattedTime}
-                      </td>
-                      <td className="py-4 px-6 text-center text-black text-lg font-medium leading-relaxed">
-                        {reservation.guests_count}
-                      </td>
-                      <td className="py-4 px-6 text-center">
-                        <span
-                          className={`flex items-center justify-center ${statusClass} text-xs font-medium mr-2 px-1.5 rounded-full py-1`}
+                    <div className="mt-4 flex items-center justify-between">
+                      <Link to={`/history-details/${reservation._id}`}>
+                        <button className="p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300 ease-in-out">
+                          <i className="fas fa-info-circle"></i> Chi Tiết
+                        </button>
+                      </Link>
+                      {reservation.status === "ISWAITING" && (
+                        <button
+                          onClick={() =>
+                            handleCancelReservation(reservation._id)
+                          }
+                          className="p-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition duration-300 ease-in-out"
                         >
-                          {getStatusInVietnamese(reservation.status)}
-                        </span>
-                      </td>
-                      <td className="py-4 px-6 text-center">
-                        <div className="flex items-center justify-center space-x-4">
-                          {/* Nếu trạng thái là ISWAITING, hiển thị nút Hủy và Xem Chi Tiết */}
-                          {reservation.status === "ISWAITING" && (
-                            <>
-                              <button
-                                onClick={() => handleCancelReservation(reservation._id)}
-                                className="p-2 bg-red-500 text-white rounded hover:bg-red-600 transition duration-300 ease-in-out"
-                              >
-                                Hủy
-                              </button>
-                              <Link to={`/history-details/${reservation?._id}`}>
-                                <button className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 ease-in-out">
-                                  Xem Chi Tiết
-                                </button>
-                              </Link>
-                            </>
-                          )}
-                          {/* Nếu trạng thái là IS_PAYMENT, hiển thị nút Tiếp tục thanh toán */}
-                          {(reservation.status === "ISWAITING" || reservation.status === "ISCOMFIRMED") && reservation.deposit == 0 && (
-                            <button
-                              onClick={() => handleBack(reservation.code)}
-                              className="p-2 bg-green-500 text-white rounded hover:bg-yellow-600 transition duration-300 ease-in-out"
-                            >
-                              Thanh toán
-                            </button>
-                          )}
-                          {/* Nếu trạng thái là các giá trị khác, chỉ hiển thị nút Xem Chi Tiết */}
-                          {reservation.status !== "ISWAITING" && reservation.status !== "ISPAYMENT" && (
-                            <Link to={`/history-details/${reservation?._id}`}>
-                              <button className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 ease-in-out">
-                                Xem Chi Tiết
-                              </button>
-                            </Link>
-                          )}
-                        </div>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                          <i className="fas fa-times"></i> Hủy
+                        </button>
+                      )}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           )}
         </div>
       </section>
--- src/pages/home/HistoryReservationDetails.jsx
+++ src/pages/home/HistoryReservationDetails.jsx
@@ -10,7 +10,9 @@ export const HistoryReservationDetail = () => {
   const [translateY] = useState(0);
   const { reservation_id } = useParams();
   const fetchReservationDetails = async () => {
-    const response = await axios.get(`http://localhost:1111/api/reservations/history-detail/${reservation_id}`);
+    const response = await axios.get(
+      `http://localhost:1111/api/reservations/history-detail/${reservation_id}`
+    );
     return response.data;
   };
   const {
@@ -30,7 +32,10 @@ export const HistoryReservationDetail = () => {
   }
   const date = new Date(reservationDetails.startTime);
   const formattedDate = date.toLocaleDateString("vi-VN");
-  const formattedTime = date.toLocaleTimeString("vi-VN", { hour: "2-digit", minute: "2-digit" });
+  const formattedTime = date.toLocaleTimeString("vi-VN", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   return (
     <div>
       <div className="relative w-full h-[200px] overflow-hidden">
@@ -51,11 +56,15 @@ export const HistoryReservationDetail = () => {
             transition: "opacity 0.3s, transform 0.3s",
           }}
         >
-          <h1 className="text-4xl md:text-5xl sm:text-3xl dancing">Chi tiết lịch sử đặt bàn</h1>
+          <h1 className="text-4xl md:text-5xl sm:text-3xl dancing">
+            Chi tiết lịch sử đặt bàn
+          </h1>
           <p className="text-4xl md:text-xl sm:text-xl mt-4 flex items-center justify-center">
             <span className="bg-white p-1 rounded-full ml-0 mr-0 hidden lg:block"></span>
             <span className="bg-white h-[2px] w-[100px] hidden lg:block"></span>
-            <span className="ml-4">Khám phá tất cả các lần đặt bàn trước đây của bạn</span>
+            <span className="ml-4">
+              Khám phá tất cả các lần đặt bàn trước đây của bạn
+            </span>
             <span className="bg-white h-[2px] w-[100px] ml-4 hidden lg:block"></span>
             <span className="bg-white p-1 rounded-full ml-0 mr-0 hidden lg:block"></span>
           </p>
@@ -74,94 +83,128 @@ export const HistoryReservationDetail = () => {
                     <p className="font-normal text-lg leading-8 text-gray-400 transition-all duration-500 group-hover:text-gray-700">
                       Họ tên
                     </p>
-                    <p className="font-medium text-lg leading-8 text-gray-900">{reservationDetails.userName}</p>
+                    <p className="font-medium text-lg leading-8 text-gray-900">
+                      {reservationDetails.userName}
+                    </p>
                   </div>
                   <div className="flex items-center justify-between gap-4 mb-5">
                     <p className="font-normal text-lg leading-8 text-gray-400 transition-all duration-500 group-hover:text-gray-700">
                       Số điện thoại
                     </p>
-                    <p className="font-medium text-lg leading-8 text-gray-600">{reservationDetails.phoneNumber}</p>
+                    <p className="font-medium text-lg leading-8 text-gray-600">
+                      {reservationDetails.phoneNumber}
+                    </p>
                   </div>
                   <div className="flex items-center justify-between gap-4 mb-5">
                     <p className="font-normal text-lg leading-8 text-gray-400 transition-all duration-500 group-hover:text-gray-700">
                       Ngày đặt
                     </p>
-                    <p className="font-medium text-lg leading-8 text-gray-900">{formattedDate}</p>
+                    <p className="font-medium text-lg leading-8 text-gray-900">
+                      {formattedDate}
+                    </p>
                   </div>
                   <div className="flex items-center justify-between gap-4 mb-5">
                     <p className="font-normal text-lg leading-8 text-gray-400 transition-all duration-500 group-hover:text-gray-700">
                       Giờ đặt
                     </p>
-                    <p className="font-medium text-lg leading-8 text-gray-900">{formattedTime}</p>
+                    <p className="font-medium text-lg leading-8 text-gray-900">
+                      {formattedTime}
+                    </p>
                   </div>
                   <div className="flex items-center justify-between gap-4 mb-5">
                     <p className="font-normal text-lg leading-8 text-gray-400 transition-all duration-500 group-hover:text-gray-700">
                       Số người
                     </p>
-                    <p className="font-medium text-lg leading-8 text-gray-900">{reservationDetails.guests_count}</p>
+                    <p className="font-medium text-lg leading-8 text-gray-900">
+                      {reservationDetails.guests_count}
+                    </p>
                   </div>
                   <div className="flex items-center justify-between gap-4 mb-5">
                     <p className="font-normal text-lg leading-8 text-gray-400 transition-all duration-500 group-hover:text-gray-700">
                       Cọc trước
                     </p>
-                    <p className="font-medium text-lg leading-8 text-gray-900">{formatCurrency(reservationDetails.deposit)}</p>
+                    <p className="font-medium text-lg leading-8 text-gray-900">
+                      {formatCurrency(reservationDetails.deposit)}
+                    </p>
                   </div>
                   <div className="flex items-center justify-between gap-4 mb-5">
                     <p className="font-normal text-lg leading-8 text-gray-400 transition-all duration-500 group-hover:text-gray-700">
                       Phiểu giảm giá
                     </p>
-                    <p className="font-medium text-lg leading-8 text-gray-900">{reservationDetails.userDiscountId?.code}</p>
+                    <p className="font-medium text-lg leading-8 text-gray-900">
+                      {reservationDetails.userDiscountId?.code}
+                    </p>
                   </div>
                   <div className="flex items-center justify-between gap-4 mb-5">
                     <p className="font-normal text-lg leading-8 text-gray-400 transition-all duration-500 group-hover:text-gray-700">
                       Đã sử dụng phiếu
                     </p>
 
-                    <p className="font-medium text-lg leading-8 text-gray-900">{reservationDetails.isUsedDiscount ? 
-                      <span className="px-2 py-1 bg-red-1 text-white rounded-md">Đã sử dụng</span> :
-                      <span className="px-2 py-1 bg-blue-1 text-white rounded-md">Chưa sử dụng</span>}
+                    <p className="font-medium text-lg leading-8 text-gray-900">
+                      {reservationDetails.isUsedDiscount ? (
+                        <span className="px-2 py-1 bg-red-1 text-white rounded-md">
+                          Đã sử dụng
+                        </span>
+                      ) : (
+                        <span className="px-2 py-1 bg-blue-1 text-white rounded-md">
+                          Chưa sử dụng
+                        </span>
+                      )}
                     </p>
-                    
                   </div>
                   <div className="flex items-center justify-between gap-4 mb-5">
                     <p className="font-normal text-lg leading-8 text-gray-400 transition-all duration-500 group-hover:text-gray-700">
                       Hình thức thanh toán
                     </p>
-                    <p className="font-medium text-lg leading-8 text-gray-900">{reservationDetails.payment_method}</p>
+                    <p className="font-medium text-lg leading-8 text-gray-900">
+                      {reservationDetails.payment_method}
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
             <div className="w-full max-w-sm md:max-w-3xl max-xl:mx-auto">
               <div className="grid grid-cols-1 gap-6">
-                {Array.isArray(reservationDetails.ordered_dishes) && reservationDetails.ordered_dishes.length > 0 ? (
-                  reservationDetails.ordered_dishes.map((orderedDish, index) => (
-                    <div
-                      key={orderedDish._id}
-                      className="rounded-3xl p-6 bg-gray-100 border border-gray-100 flex flex-col md:flex-row md:items-center gap-5 transition-all duration-500 hover:border-gray-400"
-                    >
-                      <div className="img-box">
-                        <img
-                          src={orderedDish.dish_id.images[0]}
-                          alt={orderedDish.dish_id.name}
-                          className="w-full md:max-w-[122px] rounded-lg object-cover"
-                        />
-                      </div>
-                      <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-3 md:gap-8">
-                        <div>
-                          <h2 className="font-medium text-xl leading-8 text-black mb-3">{orderedDish.dish_id.name}</h2>
+                {Array.isArray(reservationDetails.ordered_dishes) &&
+                reservationDetails.ordered_dishes.length > 0 ? (
+                  reservationDetails.ordered_dishes.map(
+                    (orderedDish, index) => (
+                      <div
+                        key={orderedDish._id}
+                        className="rounded-3xl p-6 bg-gray-100 border border-gray-100 flex flex-col md:flex-row md:items-center gap-5 transition-all duration-500 hover:border-gray-400"
+                      >
+                        <div className="img-box">
+                          <img
+                            src={orderedDish.dish_id.images[0]}
+                            alt={orderedDish.dish_id.name}
+                            className="w-full md:max-w-[122px] rounded-lg object-cover"
+                          />
                         </div>
-                        <div className="flex items-center justify-between gap-8">
-                          <h6 className="font-medium text-xl leading-8 text-600">Số lượng: {orderedDish.quantity}</h6>
-                          <h6 className="font-medium text-xl leading-8 text-600">
-                            {orderedDish.dish_id.price.toLocaleString("vi-VN", { style: "currency", currency: "VND" })}
-                          </h6>
+                        <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-3 md:gap-8">
+                          <div>
+                            <h2 className="font-medium text-xl leading-8 text-black mb-3">
+                              {orderedDish.dish_id.name}
+                            </h2>
+                          </div>
+                          <div className="flex items-center justify-between gap-8">
+                            <h6 className="font-medium text-xl leading-8 text-600">
+                              Số lượng: {orderedDish.quantity}
+                            </h6>
+                            <h6 className="font-medium text-xl leading-8 text-600">
+                              {orderedDish.dish_id.price.toLocaleString(
+                                "vi-VN",
+                                { style: "currency", currency: "VND" }
+                              )}
+                            </h6>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  ))
+                    )
+                  )
                 ) : (
-                  <div className="text-center text-gray-600">Không có món ăn đặt trước.</div>
+                  <div className="text-center text-gray-600">
+                    Không có món ăn đặt trước.
+                  </div>
                 )}
               </div>
             </div>
@@ -171,8 +214,16 @@ export const HistoryReservationDetail = () => {
               onClick={handleGoBack}
               className="rounded-full py-4 w-full max-w-[280px] flex items-center bg-indigo-50 justify-center transition-all duration-500 hover:bg-indigo-100"
             >
-              <span className="px-2 font-semibold text-lg leading-8 text-indigo-600">Quay lại</span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+              <span className="px-2 font-semibold text-lg leading-8 text-indigo-600">
+                Quay lại
+              </span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="22"
+                height="22"
+                viewBox="0 0 22 22"
+                fill="none"
+              >
                 <path
                   d="M8.25324 5.49609L13.7535 10.9963L8.25 16.4998"
                   stroke="#4F46E5"
