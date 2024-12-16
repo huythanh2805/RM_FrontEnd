0952dde5efb1943ac4814b16dbc51cabdb05ebf7
caosontr <caosontr@gmail.com>
2024-12-17 00:01:06 +0700

    Fix Width Banner, Ui Mobile Profile page

--- src/pages/home/HistoryReservation.jsx
+++ src/pages/home/HistoryReservation.jsx
@@ -131,7 +131,7 @@ export const HistoryReservation = () => {
     <div className="w-full relative">
       {/* Banner */}
 
-      <div className="relative w-full h-[250px] sm:h-[300px] lg:h-[400px] overflow-hidden">
+      <div className="relative w-full h-[150px] sm:h-[200px] lg:h-[300px] overflow-hidden">
         <div
           className="absolute inset-0 bg-cover bg-center"
           style={{
