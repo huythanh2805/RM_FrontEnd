db880c5e337894077fe4169fe8c28cb88f87a96e
Uyenlun113 <thiuyen1132004@gmail.com>
2024-12-20 00:32:08 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
